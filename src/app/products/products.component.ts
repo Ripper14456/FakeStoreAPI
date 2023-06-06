ceb790e3e15dc8db92810b6dd9286fa51ca7fce8
Reaper <reaper144@ukr.net>
2023-06-06 16:46:58 +0300

    fix second click sort, make sorting, getCategories, categories sorting

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -10,9 +10,12 @@ import {HttpClient} from "@angular/common/http";
 export class ProductsComponent {
   constructor(private http: HttpClient) {}
   products: any = [];
-
+  categories: any = [];
+  sortDesc: boolean = false;
+  sortAsc: boolean = true;
   ngOnInit() {
     this.getData();
+    this.getCategories();
   }
 
   getData() {
@@ -27,12 +30,53 @@ export class ProductsComponent {
     );
   }
 
-  Sort()
-  {
+  SortDesc() {
     this.http.get<Object[]>('https://fakestoreapi.com/products?sort=desc').subscribe(
       (data) => {
+        console.log(data);
+        this.sortDesc = true;
+        this.sortAsc = false;
         this.products = data;
-        console.log(this.products);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  SortAsc() {
+    this.http.get<Object[]>('https://fakestoreapi.com/products?sort=asc').subscribe(
+      (data) => {
+        console.log(data);
+        this.sortAsc = true;
+        this.sortDesc = false;
+        this.products = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  getCategories()
+  {
+    this.http.get<Object[]>('https://fakestoreapi.com/products/categories').subscribe(
+      (data) => {
+        this.categories = data;
+        console.log(this.categories);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  SelectCategory(Category: string)
+  {
+    this.http.get<Object[]>('https://fakestoreapi.com/products/category/' + Category).subscribe(
+      (data) => {
+        this.products = data;
+        console.log(this.categories);
       },
       (error) => {
         console.log(error);
